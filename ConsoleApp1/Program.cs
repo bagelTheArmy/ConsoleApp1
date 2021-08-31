12209f3779c6edbd877dfc89536c0bff06ebc8c4
bagelTheArmy <80268894+bagelTheArmy@users.noreply.github.com>
2021-08-31 14:00:43 -0400

    Task 1 2 and 3 are complete and everything is inside a loop much cleaner and the 1 character less bug fixed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,39 +6,172 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
+            //asks the user what they would like to do and gives brings them to the task if they hit q they quit
             bool Exit = false;
-            Console.WriteLine("0");
             while (Exit == false)
             {
-                Console.WriteLine("What Task?");
+                Console.WriteLine("Enter 1 2 or 3 for task  1,2,3 opr Q for Quit");
                 string TheTask = Console.ReadLine();
                 TheTask = TheTask.ToUpper();
-                /*switch (TheTask)
+                switch (TheTask)
                 {
-                    case '1':
+                    case "1":
                         Task1();
-                    case  "2":
+                        break;
+                    case "2":
                         Task2();
+                        break;
                     case "3":
                         Task3();
+                        break;
                     case "Q":
                         Exit = true;
-                }*/
+                        break;
+                    default:
+                        Console.WriteLine("bad input");
+                        break;
+                }
             }
-            Console.WriteLine("1");
-            for (; ; );
+
         }
         static void Task1()
         {
+            //goes through a loop unless user hits q
+            bool quit = false;
+            while (quit == false)
+            {
+                //variables
+                Console.WriteLine("Enter a String between 5 to 10 characters with one uppercase letter and one lowercase");
+                string UserInputtedString = Console.ReadLine();
+                string newString = "";
+                bool contains1Upper = false;
+                bool contains1Lower = false;
+
+                if (UserInputtedString == "Q")
+                {
+                    quit = true;
+                }
+                //if user does not hit q check to see if input is correct if it is make upper characters lower and lower characters upper else say "bad input"
+                else
+                {
+                    if (UserInputtedString.Length >= 5 && UserInputtedString.Length <= 10)
+                    {
+                        for (int i = 0; i < UserInputtedString.Length; i++)
+                        {
+                            if (64 < UserInputtedString[i] && UserInputtedString[i] < 91)
+                            {
+                                int theCharacter = UserInputtedString[i] + 32;
+                                newString += (char)theCharacter;
+                                contains1Upper = true;
+                            }
+                            if (97 <= UserInputtedString[i] && UserInputtedString[i] <= 122)
+                            {
+                                int theCharacter = UserInputtedString[i] - 32;
+                                newString += (char)theCharacter;
+                                contains1Lower = true;
+                            }
+                        }
+                        if (contains1Upper && contains1Lower)
+                        {
+                            Console.WriteLine(newString);
+                        }
+                        else
+                        {
+                            Console.WriteLine("bad input");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("bad input");
+                    }
+                }
+            }
 
         }
         static void Task2()
         {
-
+            //goes until user hits q
+            bool quit = false;
+            while (quit == false)
+            {
+                Console.WriteLine("Enter a String between 8 to 20 characters");
+                string UserInputtedString = Console.ReadLine();
+                string newString = "";
+                if (UserInputtedString == "Q")
+                {
+                    quit = true;
+                }
+                //if user does not hit q verify input. If  is input good write new string in reverse else write "bad input"
+                else
+                {
+                    if (UserInputtedString.Length >= 8 && UserInputtedString.Length <= 20)
+                    {
+                        newString = reversedTheString(UserInputtedString);
+                        Console.WriteLine(newString);
+                    }
+                    else
+                    {
+                        Console.WriteLine("bad input");
+                    }
+                }
+            }
         }
         static void Task3()
         {
+            //if user hits q quit otherwise continue through the loop
+            bool quit = false;
+            while (quit == false)
+            {
+                Console.WriteLine("Enter a number that is 3 digits long");
+                string UserInputtedString = Console.ReadLine();
+                string reversedString = "";
+                string newString = "";
+                if (UserInputtedString == "Q")
+                {
+                    quit = true;
+                }
+                //make every number 1 higher and reverse it if input is good else give bad input
+                else
+                {
+                    if (UserInputtedString.Length == 3)
+                    {
+                        try
+                        {
+                            int number = Int32.Parse(UserInputtedString);
+                            for (int i = 0; i < UserInputtedString.Length; i++)
+                            {
+                                int num = number % 10;
+                                num += 1;
+                                reversedString = reversedString + num.ToString();
+                                number = number / 10;
+                            }
+                            newString = reversedTheString(reversedString);
+                            Console.WriteLine(newString);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("bad input");
+                        }
 
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("bad input");
+                    }
+                }
+            }
+        }
+
+        static string reversedTheString(string stringToReverse)
+        {
+            //reverses the string
+            string newString = "";
+            for (int i = stringToReverse.Length - 1; i >= 0; i--)
+            {
+                newString += stringToReverse[i];
+            }
+            return newString;
         }
 
 
